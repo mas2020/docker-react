a002a3d105119d14347b1ac5ef6ac5d71dc21c08
Andrea Genovesi <andrea.genovesi@gmail.com>
2020-02-25 10:16:54 +0100

    changed Dockerfile to get IMG from node:alpine

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM frontend-test:latest as builder
+FROM node:alpine as builder
 WORKDIR /app
 COPY package.json .
 RUN npm install
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there!!
+          Hello there!! version 1.1
         </p>
         <a
           className="App-link"
